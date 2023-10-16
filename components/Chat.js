374d228a29fc2ffac878cfc70431e3f7bdf2edf9
elitemagic <toddmerlin@hotmail.com>
2023-10-16 15:57:46 +1100

    Added messenger functionality with GIftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,61 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, background } = route.params;
 
+  /* initailze state */
+  const [messages, setMessages] = useState([]);
+
+  // useEffect(() => {
+  //   navigation.setOptions({ title: name });
+  // }, []);
+
+  /* Function called right after the Chat component mounts - use setter method to see each element of the UI displayed on the screen right away*/
   useEffect(() => {
-    navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
   }, []);
+
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
 
   return (
     <View style={[styles.container, { backgroundColor: background }]}>
-      <Text>Hello, and welcome to Screen 2!</Text>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === "ios" ? (
+        <KeyboardAvoidingView behavior="padding" />
+      ) : null}
+
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
     </View>
   );
 };
@@ -18,8 +63,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
